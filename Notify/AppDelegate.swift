16b26c4d4ef3d0286d42b36fc4aa34c19b6ff1f3
David Aghassi <david.aghassi@gmail.com>
2015-09-18 12:00:45 -0400

    Initial Commit

---
+++ Notify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notify
+//
+//  Created by David Aghassi on 9/18/15.
+//  Copyright © 2015 David Aghassi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Notify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Notify
+//
+//  Created by David Aghassi on 9/18/15.
+//  Copyright © 2015 David Aghassi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
