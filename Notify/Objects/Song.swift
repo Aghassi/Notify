8a2d8bb55f6159ecb4e1f26b8a604dfb5bdbbc75
David Aghassi <david.aghassi@gmail.com>
2015-09-19 21:05:34 -0400

    Added Song.swift
    - Defined Song.swift object

---
+++ Notify/Objects/Song.swift
@@ -0,0 +1,22 @@
+//
+//  Song.swift
+//  Notify
+//
+//  Created by David Aghassi on 9/19/15.
+//  Copyright © 2015 David Aghassi. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class Song: NSObject {
+    /*
+    Outline the parameters a song object can store
+    */
+    var id = "",
+        name = "",
+        artist = "",
+        album = "",
+        trackID = 0,
+        image = NSImage()
+}
