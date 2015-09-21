0ed8a3538385e99e6e3622fa44026ad16401360d
David Aghassi <david.aghassi@gmail.com>
2015-09-21 19:43:49 -0400

    Updated widget layout
    -  Updated constraints
    -  Made outlets
    -  Linked Assets.images to notification app

--- NCWidget/TodayViewController.swift
+++ NCWidget/TodayViewController.swift
@@ -10,7 +10,21 @@ import Cocoa
 import NotificationCenter
 
 class TodayViewController: NSViewController, NCWidgetProviding {
+    @IBOutlet weak var albumCoverImage: NSImageView!
 
+    @IBOutlet weak var songLabel: NSTextField!
+    @IBOutlet weak var artistLabel: NSTextField!
+    @IBOutlet weak var albumLabel: NSTextField!
+    
+    
+    @IBAction func playButton(sender: NSButton) {
+    }
+    
+    @IBAction func fastForwardButton(sender: NSButton) {
+    }
+    @IBAction func rewindButton(sender: NSButton) {
+    }
+    
     override var nibName: String? {
         return "TodayViewController"
     }
@@ -20,6 +34,12 @@ class TodayViewController: NSViewController, NCWidgetProviding {
         // with NoData if nothing has changed or NewData if there is new data since the last
         // time we called you
         completionHandler(.NoData)
+    }
+    
+    override func viewDidLoad() {
+        songLabel.stringValue = ""
+        artistLabel.stringValue = ""
+        albumLabel.stringValue = ""
     }
 
 }
