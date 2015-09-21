4bf5c160d51e547768e1c69080ddda30700786e1
Chris Rees <serneum@gmail.com>
2015-09-21 00:10:25 -0400

    Add Play/Pause functionality

--- Notify/AppDelegate.swift
+++ Notify/AppDelegate.swift
@@ -52,6 +52,11 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         sendSpotifyCommand("previous")
     }
     
+    // Would like to change the menu item text if we can tell if spotify is playing
+    @IBAction func playPauseToggle(sender: NSMenuItem) {
+        sendSpotifyCommand("playpause")
+    }
+    
     func sendSpotifyCommand(command: String) {
         let script = "tell application \"Spotify\"\n" +
             command + " track\n" +
