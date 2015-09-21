036ed6ff1319c29e3380b11a9fd24cfe33cc855d
David Aghassi <david.aghassi@gmail.com>
2015-09-21 17:52:03 -0400

    Started development of Today Extension
    -  Laid out general controls
    -  Laid out general data format

---
+++ NCWidget/TodayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TodayViewController.swift
+//  NCWidget
+//
+//  Created by David Aghassi on 9/21/15.
+//  Copyright © 2015 David Aghassi. All rights reserved.
+//
+
+import Cocoa
+import NotificationCenter
+
+class TodayViewController: NSViewController, NCWidgetProviding {
+
+    override var nibName: String? {
+        return "TodayViewController"
+    }
+
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Update your data and prepare for a snapshot. Call completion handler when you are done
+        // with NoData if nothing has changed or NewData if there is new data since the last
+        // time we called you
+        completionHandler(.NoData)
+    }
+
+}
